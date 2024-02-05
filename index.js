3f5be5f88956d778caf63a4b0b4344e674446864
Areliya13 <sharipovatamara@gmail.com>
2024-02-06 00:53:30 +0300

    feat: implemented exit

---
+++ index.js
@@ -0,0 +1,39 @@
+import { getUserName } from "./src/userName.js";
+import { createInterface } from "readline";
+import { stdin as input, stdout as output, chdir, cwd } from "node:process";
+import os from "node:os";
+
+const currentState = {
+    userName: "",
+};
+
+const exit = () => {
+    console.log(`Thank you for using File Manager, ${currentState.userName}, goodbye!`);
+    process.exit(1);
+};
+
+const start = async () => {
+    currentState.userName = getUserName(process.argv) || "User";
+    console.log(`Welcome to the File Manager, ${currentState.userName}!`);
+    const readline = createInterface({ input, output });
+    readline.write(chdir(os.homedir()));
+    console.log(`You are currently in ${cwd()}`);
+
+    readline.on("line", async (line) => {
+        const [command, arg1, arg2] = line.trim().split(" ");
+
+        switch (command) {
+            case ".exit":
+                exit();
+                break;
+            default:
+                break;
+        }
+
+        console.log(`You are currently in ${process.cwd()}`);
+    });
+
+    readline.on("SIGINT", exit);
+};
+
+start();
