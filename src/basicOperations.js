03344eee03b2aac796f6ddb00efd5229200d452d
Areliya13 <sharipovatamara@gmail.com>
2024-02-06 05:17:31 +0300

    refactor: update implemented basic operations

--- src/basicOperations.js
+++ src/basicOperations.js
@@ -1,59 +1,28 @@
-import process from "node:process";
+import { cwd, stdout } from "node:process";
 import { access, constants, rename, writeFile } from "node:fs/promises";
 import { createReadStream, createWriteStream } from "node:fs";
 import { extname, join, parse } from "node:path";
+import { isFile, resolvePath } from "./utils.js";
+import { messages } from "./messages.js";
+import { pipeline } from "node:stream/promises";
 
-export const cat = async (pathToFile) => {
-    const isFile = extname(pathToFile);
-    if (!isFile) {
-        console.log("Wrong file name");
-        return;
-    }
-    try {
-        await access(pathToFile, constants.R_OK);
-    } catch (e) {
-        console.log("Can't read the file");
-        return;
-    }
-
-    try {
-        const readableStream = createReadStream(pathToFile);
-        readableStream.pipe(process.stdout);
-    } catch (e) {
-        console.log("Operation cat failed");
-    }
+export const cat = async (fileName) => {
+    const pathToFile = resolvePath(cwd(), fileName);
+    return new Promise((res, rej) => {
+        const readableStream = createReadStream(pathToFile, "utf8");
+        readableStream.on("error", rej);
+        readableStream.on("data", console.log);
+        readableStream.on("end", res);
+    });
 };
 
 export const add = async (fileName) => {
-    const isFile = extname(fileName);
-    if (!isFile) {
-        console.log("Wrong file name");
-        return;
-    }
-
-    const pathToFile = join(process.cwd(), fileName);
-
-    let fileExist = false;
-    try {
-        await access(pathToFile, constants.W_OK);
-        fileExist = true;
-    } catch (e) {}
-
-    if (fileExist) {
-        console.log("Operation add failed");
-        return;
-    }
-
-    await writeFile(pathToFile, "");
-    console.log(`File ${fileName} created`);
+    const pathToFile = resolvePath(cwd(), fileName);
+    await writeFile(pathToFile, "", { flag: "wx" });
 };
 
-export const rn = async (pathToFile, newFilename) => {
-    const newPathToFile = join(parse(pathToFile).dir, newFilename);
-    try {
-        await rename(pathToFile, newPathToFile);
-        console.log(`File ${pathToFile} renamed to ${newFilename}`);
-    } catch (e) {
-        console.log("Operation rn failed");
-    }
+export const rn = async (oldFileName, newFilename) => {
+    const fromPath = resolvePath(cwd(), oldFileName);
+    const toPath = resolvePath(cwd(), newFilename);
+    await rename(fromPath, toPath);
 };
